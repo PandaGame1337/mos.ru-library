2f113c1f9b277c327577ab61897b3dfadbb5d05c
PandaGame1337 <sstarkess@yandex.ru>
2021-10-23 10:04:58 +0300

    slider

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $(".js-topslider").slick({
+        arrows: true,
+        prevArrow: "<img src='/mos.ru-library/images/top-slider/arrow-left.svg' class='slider-prev' alt='prev'>",
+        nextArrow: "<img src='/mos.ru-library/images/top-slider/arrow-right.svg' class='slider-next' alt='next'>"
+    })
+});
